632cf3c29efa8fd8ce8107a0d1ad70a0ac30d601
Rima Alrwais <ralrwais@ralrwais-ltm2.internal.salesforce.com>
2017-05-05 13:58:21 -0700

    Updates

--- server.js
+++ server.js
@@ -12,6 +12,16 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.text());
 app.use(bodyParser.json({type:'application/vnd.api+json'}));
 
+
+var exphbs = require("express-handlebars");
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var routes = require("./controllers/burgers_controller.js");
+
+app.use("/", routes);
+
+
 app.listen(PORT, function() {
     console.log("Server listening on PORT: " + PORT);
 });
